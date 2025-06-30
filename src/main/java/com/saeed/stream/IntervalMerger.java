35f1a19f06e920643acc9ba7aea81268de1ea0a6
naseriboroujeni <naseriboroujeni@gmail.com>
2025-06-30 21:52:34 +0300

    Add mergeIntervals for arrays

--- src/main/java/com/saeed/stream/IntervalMerger.java
+++ src/main/java/com/saeed/stream/IntervalMerger.java
@@ -30,12 +30,31 @@ public class IntervalMerger {
         for (int i = 0; i < intervals.size(); i++) {
             int start = intervals.get(i).start;
             int end = intervals.get(i).end;
-            while ((i+1) < intervals.size() && intervals.get(i).end >= intervals.get(i+1).start) {
+            while ((i+1) < intervals.size() && end >= intervals.get(i+1).start) {
                 end = Math.max(end, intervals.get(++i).end);
             }
             mergedIntervals.add(new Interval(start, end));
         }
         return mergedIntervals;
+    }
+
+    public static int[][] mergeIntervals(int[][] intervals) {
+        Arrays.sort(intervals, Comparator.comparing(a -> a[0]));
+
+        int[][] mergedIntervals = new int[intervals.length][2];
+
+        int j = 0;
+        for (int i = 0; i < intervals.length; i++) {
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+            while ((i+1) < intervals.length && end >= intervals[i+1][0]) {
+                end = Math.max(end, intervals[++i][1]);
+            }
+            mergedIntervals[j][0] = start;
+            mergedIntervals[j][1] = end;
+            j++;
+        }
+        return Arrays.copyOfRange(mergedIntervals, 0, j);
     }
 
     public static void main(String[] args) {
@@ -49,5 +68,9 @@ public class IntervalMerger {
 
         List<Interval> result = mergeIntervals(input);
         System.out.println(result); // Expected: [(1,10), (15,18)]
+
+        int[][] intervals = {{1,4},{0,2},{3,5}};
+        int[][] result2 = mergeIntervals(intervals);
+        System.out.println(Arrays.deepToString(result2));
     }
 }
