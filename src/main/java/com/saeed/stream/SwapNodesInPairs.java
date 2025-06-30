c00ce9bc579eed639b6967be2fe4fe8756097468
naseriboroujeni <naseriboroujeni@gmail.com>
2025-06-30 23:19:45 +0300

    Move ListNode to separate file

--- src/main/java/com/saeed/stream/SwapNodesInPairs.java
+++ src/main/java/com/saeed/stream/SwapNodesInPairs.java
@@ -2,34 +2,6 @@ package com.saeed.stream;
 
 public class SwapNodesInPairs {
 
-    static class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode() {
-        }
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
-
-        @Override
-        public String toString() {
-            StringBuilder result = new StringBuilder();
-            result.append(val);
-            if (next != null) {
-                result.append(", ");
-                result.append(next);
-            }
-            return result.toString();
-        }
-    }
-
     public static ListNode swapPairs(ListNode head) {
         ListNode first = head;
         ListNode previous = null;
